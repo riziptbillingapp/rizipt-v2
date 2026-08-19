dcc3b13d76da9c74dae731ffa8d21f16960fd938
riziptbillingapp <rizipt.billingapp@gmail.com>
2026-08-19 09:55:21 +0530

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,6 +3,7 @@ const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8787";
 async function request(path, options = {}) {
   const res = await fetch(`${BASE_URL}${path}`, {
     headers: { "Content-Type": "application/json" },
+    credentials: "include",
     ...options,
   });
 
